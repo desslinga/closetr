fc72987fa4a9e8d8a0ef718cfb32996547c46b41
Fides Linga <fides.linga@mail.utoronto.ca>
2019-05-09 16:01:22 -0400

    S-80: Added documentation to login controller.

--- closetr-web/src/app/pages/page-full/login/login.component.ts
+++ closetr-web/src/app/pages/page-full/login/login.component.ts
@@ -8,6 +8,30 @@ import { first } from 'rxjs/operators';
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
+
+/*
+Page containing a login form, for a visitor to log in to the platform. Login
+form contains a username and password field, and a 'log in' button. The
+'log in' button is enabled only when both username and password fields are
+non-empty. When visitor has entered incorrect login credentials, then an
+appropriate error message is displayed.
+
+There is a 'sign up' button which links to the Register page, if a visitor
+does not have an account yet. When a user has clicked 'log in' with correct
+credentials, they are redireted to the Dashboard page.
+
+username: the input value of the username field (entered by user.)
+
+password: the input value of the password field (entered by user.)
+
+enableLogin: boolean to indicate whether or not 'log in' button should be
+enabled.
+
+showLoginError: boolean to indicate whether or not error messages should be
+displayed on the username/password fields.
+
+show: boolean to indicate whether or not login page should be displayed.
+*/
 export class LoginComponent implements OnInit {
   username: string = '';
   password: string = '';
@@ -18,38 +42,50 @@ export class LoginComponent implements OnInit {
   constructor(private router: Router,
               private authenticationService: AuthenticationService) { }
 
+  /*
+  Initial data loading: show login page. Check if there is a current user
+  already logged in, and if yes, then navigate to the Dashboard page.
+  */
   ngOnInit() {
     this.show = true;
     (this.authenticationService.currentUserValue
       && this.router.navigate(['/dashboard']));
   }
 
-  checkEnableLogin = (): boolean =>
-    this.enableLogin = !(this.username.length == 0 || this.password.length == 0);
-
+  /*
+  Called whenever the username or password fields change with user input. Any
+  error messages displayed are cleared. The enableLogin variable is updated,
+  and set to true if both the username and password fields are non-empty
+  (false otherwise).
+  */
   loginChangeHandler = (): void => {
     this.showLoginError = false;
-    this.checkEnableLogin();
+    this.enableLogin = !(this.username.length == 0 || this.password.length == 0);
   }
 
+  /*
+  Called when user clicks the 'sign up' button. Navigates to the Register page.
+  */
   toSignUp = (): Promise<boolean> => this.router.navigate(['/register']);
 
+  /*
+  Called when user clicks the 'log in' button. Wraps username and password
+  field input into a param object, and calls authentication service's login
+  method to validate the login data. The validation result is checked after
+  it is returned. If the login data is valid, then the user is redirected to
+  the dashboard. Otherwise, appropriate errors are shown on the fields.
+  */
   login(): void {
     this.showLoginError = false;
-    let loginData = {
-      userID: this.username,
-      userPassword: this.password
-    };
+    const loginData = { userID: this.username, userPassword: this.password };
     this.authenticationService.login(loginData)
-      .subscribe(
-        data => {
-          if (data) {
-            this.router.navigate(['/dashboard']);
-          } else {
-            this.showLoginError = true;
-          }
+      .subscribe(data => {
+        if (data) {
+          this.router.navigate(['/dashboard']);
+        } else {
+          this.showLoginError = true;
         }
-      );
+     });
   }
 
 }
