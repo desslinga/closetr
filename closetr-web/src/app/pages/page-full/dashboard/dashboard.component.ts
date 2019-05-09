7b6dfa4597fb5bedda03b801e74ae0262b5ba541
Fides Linga <fides.linga@mail.utoronto.ca>
2019-05-09 15:06:51 -0400

    S-80: Cleaning up and adding documentation to dashboard controller.

--- closetr-web/src/app/pages/page-full/dashboard/dashboard.component.ts
+++ closetr-web/src/app/pages/page-full/dashboard/dashboard.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../../../services/authentication.service';
-import { User } from '../../../models/user.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html'
 })
 
+/*
+Page containing all widgets in the dashboard. The dashboard is the page that
+the user is redirected to upon logging in. It contains the Today Widget,
+Closet Widget, Closet Stats Widget, and Budget Widget. Further descriptions
+about these widgets will be found in the respective controller pages for these
+widgets.
+*/
 export class DashboardComponent implements OnInit {
-  currentUser: User;
+  constructor() { }
 
-  constructor(private authenticationService: AuthenticationService) { }
-
-  ngOnInit() {
-    this.currentUser = this.authenticationService.currentUserValue;
-  }
+  ngOnInit() { }
 
 }
