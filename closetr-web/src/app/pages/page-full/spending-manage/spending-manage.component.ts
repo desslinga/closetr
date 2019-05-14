ae7fcb59b81acf4e106a30d2f1c04fef5f4896a3
Fides Linga <fides.linga@mail.utoronto.ca>
2019-05-14 16:50:06 -0400

    S-80: Adding component description to spending manage.

--- closetr-web/src/app/pages/page-full/spending-manage/spending-manage.component.ts
+++ closetr-web/src/app/pages/page-full/spending-manage/spending-manage.component.ts
@@ -12,6 +12,30 @@ import { ClosetFactory } from '../../../factories/closet.factory';
   selector: 'app-spending-manage',
   templateUrl: './spending-manage.component.html'
 })
+/*
+Page containing information about user's clothing spending. Contains an table
+of recent purchases, which can be filtered by recency (date range). There is
+also a search bar where the user can further filter the purchase table. An
+'add manually' button allows the user to navigate to the Add Clothing page.
+
+There is a progress bar that shows the user's progress through their budget for
+some set date range, and a message to reflect how their progress. Beneath the
+message is a link to the Budget Manage page.
+
+closetList: an array consisting of all the user's clothing.
+
+isDateRange: boolean indicating if the user wants to filter the purchase table
+via a date range (defining both date from and to), or a date range up to today
+(e.g. last week, last month).
+
+searchCriteria: an object describing criteria pertaining to filtering the
+purchase table, such as date to and from, etc.
+
+availableDateRange: an array of available date range options to be shown in
+the date range for selector.
+
+currentUser: the user that is logged in.
+*/
 export class SpendingManageComponent implements OnInit {
   closetList: Array<Clothing>;
   isDateRange: boolean = false;
