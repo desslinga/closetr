1b538af40a5434412108ca0b0d10cfcc98490631
Fides Linga <fides.linga@mail.utoronto.ca>
2019-05-15 12:07:44 -0400

    S-80: Adding documentation to ui-back and ui-close button controllers.

--- closetr-web/src/app/shared/ui-back-button/ui-back-button.component.ts
+++ closetr-web/src/app/shared/ui-back-button/ui-back-button.component.ts
@@ -5,14 +5,23 @@ import { RouterModule, Routes, Router } from '@angular/router';
   selector: 'ui-back-button',
   templateUrl: './ui-back-button.component.html'
 })
+/*
+Back arrow button component. Clicking this button will navigate the user to
+the given url.
+
+url: the url for the page to navigate to, when the button is clicked. Defaults
+to "/" (dashboard.)
+*/
 export class UiBackButtonComponent implements OnInit {
   @Input() url: string = "/";
 
   constructor(private router: Router) { }
 
+  /*
+  Called when user clicks the back button. Navigates to the given url.
+  */
   back = (): Promise<boolean> => this.router.navigate([this.url]);
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
--- closetr-web/src/app/shared/ui-close-button/ui-close-button.component.ts
+++ closetr-web/src/app/shared/ui-close-button/ui-close-button.component.ts
@@ -4,9 +4,24 @@ import { Component, OnInit, Input } from '@angular/core';
   selector: 'ui-close-button',
   templateUrl: './ui-close-button.component.html'
 })
+/*
+Close "x" button component. Clicking this button will trigger some function
+that exists in the parent component. Can be customized by type (depending
+on parent component), and size. Close button can also be hidden, in which case
+it will also be disabled.
+
+type: how the button will be displayed. Defaults to 'button', with no
+adjustments to positioning. If type is 'closet-card', then button's position
+will be adjusted to fit properly on a Closet Card component.
+
+hidden: boolean indicating whether the button will be displayed or not. If
+it is hidden, it is also disabled.
+
+size: size of the button icon. Defaults to 'lg' (large). Other options are
+'md' (medium). See ui-icon-sized component for how size affects the icon.
+*/
 export class UiCloseButtonComponent implements OnInit {
   @Input() type: string = 'button';
-  @Input() buttonLink: string = '/';
   @Input() hidden: boolean = false;
   @Input() size: string = 'lg';
   constructor() { }
