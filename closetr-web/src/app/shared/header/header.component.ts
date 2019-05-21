25b820f7285c3351cc83cda3c1482d1e4d333317
desslinga <fides.linga@mail.utoronto.ca>
2019-05-20 21:12:15 -0400

    S-80: Adding documentation to header component

--- closetr-web/src/app/shared/header/header.component.ts
+++ closetr-web/src/app/shared/header/header.component.ts
@@ -6,20 +6,45 @@ import { Location } from '@angular/common';
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
+/*
+Header component that is displayed on the top of web-application. Contains the
+user icon, and the Closetr icon. The header component is hidden when there is
+no user logged in -- aka the Login and Register pages. When the user icon is
+clicked, the user popup menu appears, where the user can access their profile
+and settings, or logout.
+
+currUrl: the current url that contains the page.
+
+isHidden: boolean indicating whether or not the header component should be
+hidden.
+
+showMenu: boolean indicating whether or not the user popup menu should be
+displayed.
+*/
 export class HeaderComponent implements OnInit {
-  currUrl: string;
-  isHidden: boolean;
-  showMenu: boolean;
+  currUrl: string = '';
+  isHidden: boolean = true;
+  showMenu: boolean = false;
 
   constructor(private router: Router,
-              private location: Location) {
-      router.events.subscribe((val) => {
-        this.currUrl = this.location.path();
-        this.checkHidden();
-      });
-      this.showMenu = false;
+              private location: Location) { }
+
+  /*
+  Initial data loading: obtain the current url of the page, and then check if
+  the header component should be displayed for that url.
+  */
+  ngOnInit() {
+    this.router.events.subscribe((val) => {
+      this.currUrl = this.location.path();
+      this.checkHidden();
+    });
   }
 
+  /*
+  Updates the isHidden boolean to whether the header component should be hidden
+  or not. isHidden will be true if the current url points to either of the
+  Login, or Register pages.
+  */
   checkHidden(): void {
     this.isHidden = false;
     if (this.currUrl == '/login'
@@ -29,11 +54,9 @@ export class HeaderComponent implements OnInit {
     }
   }
 
-  toggleMenu(): void {
-    this.showMenu = !this.showMenu;
-  }
-
-  ngOnInit() {
-  }
+  /*
+  Toggles the user popup menu to show or hide. 
+  */
+  toggleMenu = (): boolean => this.showMenu = !this.showMenu;
 
 }
